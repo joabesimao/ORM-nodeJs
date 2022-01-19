3d45167c17c620a1f687a336f7c3e9d514e4b2c0
joabesimao <simaojoabecosta@gmail.com>
2022-01-19 08:13:28 -0300

    criar associação com turma_id,docente_id e nivel_id

---
+++ api/models/turmas.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Turmas extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Turmas.associate = function (models) {
+        Turmas.hasMany(models.Matriculas, {
+          foreignKey: "turma_id",
+        });
+        Turmas.belongsTo(models.Pessoas, {
+          foreignKey: "docente_id",
+        });
+        Turmas.belongsTo(models.Niveis, {
+          foreignKey: "nivel_id",
+        });
+      };
+    }
+  }
+  Turmas.init(
+    {
+      data_inicio: DataTypes.DATEONLY,
+    },
+    {
+      sequelize,
+      modelName: "Turmas",
+    }
+  );
+  return Turmas;
+};
