a46614971c70431d4c48a251222c7985d392c44d
joabesimao <simaojoabecosta@gmail.com>
2022-01-19 08:05:10 -0300

    criar associação com docente_id e estudante_id

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Pessoas extends Model {
     /**
@@ -9,18 +7,26 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
+    static associate = function (models) {
+      Pessoas.hasMany(models.Turmas, {
+        foreignKey: "docente_id",
+      });
+      Pessoas.hasMany(models.Matriculas, {
+        foreignKey: "estudante_id",
+      });
+    };
   }
-  Pessoas.init({
-    nome: DataTypes.STRING,
-    ativo: DataTypes.BOOLEAN,
-    email: DataTypes.STRING,
-    role: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Pessoas',
-  });
+  Pessoas.init(
+    {
+      nome: DataTypes.STRING,
+      ativo: DataTypes.BOOLEAN,
+      email: DataTypes.STRING,
+      role: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Pessoas",
+    }
+  );
   return Pessoas;
 };
