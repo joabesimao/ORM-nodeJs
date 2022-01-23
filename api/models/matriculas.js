4021868c66564f009d3c5c4e9ac3aecb1a3cc479
joabesimao <simaojoabecosta@gmail.com>
2022-01-23 16:09:11 -0300

    criar relações com estudante_id  e turma_id

--- api/models/matriculas.js
+++ api/models/matriculas.js
@@ -8,12 +8,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Matriculas.belongsTo(models.Pessoas, {
-        foreignKey: "estudante_id",
-      });
-      Matriculas.belongsTo(models.Turmas, {
-        foreignKey: "turma_id",
-      });
+      Matriculas.associate = function (models) {
+        Matriculas.belongsTo(models.Pessoas, {
+          foreignKey: "estudante_id",
+        });
+        Matriculas.belongsTo(models.Turmas, {
+          foreignKey: "turma_id",
+        });
+      };
     }
   }
   Matriculas.init(
