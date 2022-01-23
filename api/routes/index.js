916ee761842abe83014058c4da8cb15fa2fbfd22
joabesimao <simaojoabecosta@gmail.com>
2022-01-23 16:16:29 -0300

    adicionar as importações de niveis pessoas e turmas

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,7 +1,16 @@
 const bodyParser = require("body-parser");
+
 const pessoas = require("./pessoasRoute");
 
+const niveis = require("./niveisRoute");
+const turmas = require("./turmasRoute");
+
 module.exports = (app) => {
-  app.use(bodyParser.json());
-  app.use(pessoas);
+  app.use(
+    bodyParser.json(),
+    bodyParser.urlencoded({ extended: false }),
+    pessoas,
+    niveis,
+    turmas
+  );
 };
