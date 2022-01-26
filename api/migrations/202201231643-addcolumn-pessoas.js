8fdc8c79fcbfb7b139fd01fac8494238f04c5c75
joabesimao <simaojoabecosta@gmail.com>
2022-01-26 08:47:06 -0300

    adicionar coluna deletedAt

---
+++ api/migrations/202201231643-addcolumn-pessoas.js
@@ -0,0 +1,12 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.addColumn("Pessoas", "deletedAt", {
+      allowNull: true,
+      type: Sequelize.DATE,
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.removeColumn("Pessoas", "deletedAt");
+  },
+};
