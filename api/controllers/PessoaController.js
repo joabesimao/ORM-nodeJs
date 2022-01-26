aeb58d62a87414c3c54d6a62e5b6c2e2e4cc1feb
joabesimao <simaojoabecosta@gmail.com>
2022-01-26 16:01:33 -0300

    adicionar o metodo pegaMatriculaPorTurma

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -137,6 +137,20 @@ class PessoaController {
       return res.status(500).json(error.message);
     }
   }
+
+  static async pegaMatriculaPorTurma(req, res) {
+    const { turmaId } = req.params;
+    try {
+      const todasAsMatriculas = await database.Matriculas.findAndCountAll({
+        where: { turma_id: Number(turmaId), status: "confirmado" },
+        limit: 20,
+        order: [["estudante_id", "ASC"]],
+      });
+      return res.status(200).json(todasAsMatriculas);
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
 }
 
 module.exports = PessoaController;
