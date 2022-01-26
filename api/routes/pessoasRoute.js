72d7f8409725a1e05e34542f2730655d56f2cf70
joabesimao <simaojoabecosta@gmail.com>
2022-01-26 17:11:28 -0300

    adicionar post restauraPessoa,e pegaUmaMatricula

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,7 +4,8 @@ const PessoaController = require("../controllers/PessoaController");
 const router = Router();
 
 router
-  .get("/pessoas", PessoaController.pegaTodasAsPessoas)
+  .get("/pessoas", PessoaController.pegaPessoasAtivas)
+  .get("/pessoas/todos", PessoaController.pegaTodasAsPessoas)
   .get("/pessoas/:id", PessoaController.pegaUmaPessoa)
   .post("/pessoas", PessoaController.criaPessoa)
   .put("/pessoas/:id", PessoaController.atualizaPessoa)
@@ -13,6 +14,8 @@ router
     "/pessoas/:estudanteId/matriculas/:matriculaId",
     PessoaController.pegaUmaMatricula
   )
+  .get("/pessoas/:estudanteId/matricula", PessoaController.pegaMatricula)
+  .post("/pessoas/:id/restaura", PessoaController.restauraPessoa)
   .post("/pessoas/:estudanteId/matriculas", PessoaController.criaNovaMatricula);
 
 module.exports = router;
