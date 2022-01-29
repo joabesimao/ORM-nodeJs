2e42ad580d632b7a113f1f7feda1488d6b92fce2
joabesimao <simaojoabecosta@gmail.com>
2022-01-29 18:59:09 -0300

    criar Niveisservices

---
+++ api/services/PessoasServices.js
@@ -0,0 +1,9 @@
+const Services = require("./Services");
+
+class PessoasServices extends Services {
+  constructor() {
+    super("Pessoas");
+  }
+}
+
+module.exports = PessoasServices;
---
+++ api/services/index.js
@@ -0,0 +1,9 @@
+const PessoasServices = require("./PessoasServices");
+const TurmasServices = require("./TurmaServices");
+const NiveisServices = require("./NiveisServices");
+
+module.exports = {
+  PessoasServices: PessoasServices,
+  TurmasServices: TurmasServices,
+  NiveisServices: NiveisServices,
+};
