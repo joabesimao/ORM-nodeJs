e3d3e97084f1258932d625680ba84ea5d5957b58
joabesimao <simaojoabecosta@gmail.com>
2022-01-30 14:50:53 -0300

    criar index de services

--- api/services/index.js
+++ api/services/index.js
@@ -1,9 +1,11 @@
 const PessoasServices = require("./PessoasServices");
 const TurmasServices = require("./TurmaServices");
 const NiveisServices = require("./NiveisServices");
+const MatriculasServices = require("./MatriculasServices");
 
 module.exports = {
   PessoasServices: PessoasServices,
   TurmasServices: TurmasServices,
   NiveisServices: NiveisServices,
+  MatriculasServices: MatriculasServices,
 };
