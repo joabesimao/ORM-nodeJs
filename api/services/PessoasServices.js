88e52c7850e13766e0cb421341fdb54bfd615457
joabesimao <simaojoabecosta@gmail.com>
2022-01-30 14:49:15 -0300

    criar novos Metodos

--- api/services/PessoasServices.js
+++ api/services/PessoasServices.js
@@ -1,8 +1,33 @@
 const Services = require("./Services");
+const database = require("../models");
 
 class PessoasServices extends Services {
   constructor() {
     super("Pessoas");
+    this.matriculas = new Services("Matriculas");
+  }
+
+  async pegaRegistrosAtivos(where = {}) {
+    return database[this.nomeDoModelo].findAll({ where: { ...where } });
+  }
+
+  async pegaTodosOsRegistros(where = {}) {
+    return database[this.nomeDoModelo]
+      .scope("todos")
+      .findAll({ where: { ...where } });
+  }
+
+  async cancelaPessoasEMatriculas(estudanteId) {
+    return database.sequelize.transaction(async (transacao) => {
+      await super.atualizaRegistro({ ativo: false }, estudanteId, {
+        transaction: transacao,
+      });
+      await this.matriculas.atualizaRegistro(
+        { status: "cancelado" },
+        { estudante_id: estudanteId },
+        { transaction: transacao }
+      );
+    });
   }
 }
 
