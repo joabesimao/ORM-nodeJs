0daa0123a9a207c426e2c959e2473189607ec158
joabesimao <simaojoabecosta@gmail.com>
2022-01-30 14:50:05 -0300

    criar metodo criaRegistro

--- api/services/Services.js
+++ api/services/Services.js
@@ -8,6 +8,30 @@ class Services {
   async pegaTodosOsRegistros() {
     return database[this.nomeDoModelo].findAll();
   }
+
+  async pegaUmRegistroAtivo(where = {}) {
+    return database[this.nomeDoModelo].findOne({ where: { ...where } });
+  }
+
+  async criaRegistro(dados) {
+    return database[this.nomeDoModelo].create(dados);
+  }
+
+  async atualizaRegistro(dadosAtualizados, id) {
+    return database[this.nomeDoModelo].update(
+      dadosAtualizados,
+      { where: { id: id } },
+      transacao
+    );
+  }
+
+  async atualizaRegistros(dadosAtualizados, where, transacao = {}) {
+    return database[this.nomeDoModelo].update(
+      dadosAtualizados,
+      { where: { ...where } },
+      transacao
+    );
+  }
 }
 
 module.exports = Services;
